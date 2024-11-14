1e3fbbd215e833af81bcdf0139b7395b433943d1
abdulkasif <abdulkasif95@gmail.com>
2024-11-14 22:46:26 +0530

    configured folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div className="flex bg-red-300 text-blue-500 text-4xl text-2xl min-h-screen ">
+      <div className="flex bg-red-300 text-blue-500 text-4xl min-h-screen ">
         <h1>Hello World</h1>
       </div>
 
